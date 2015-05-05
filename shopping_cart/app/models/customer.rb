87e64b54fce001fcd2a0b2f75b36e317c1fbaaab
Cara Hill <chilly23@gmail.com>
2015-05-06 11:17:40 +1200

    Added order model and created associations.

--- shopping_cart/app/models/customer.rb
+++ shopping_cart/app/models/customer.rb
@@ -3,4 +3,7 @@ class Customer < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :orders
+  has_many :products, through: :orders
 end
---
+++ shopping_cart/app/models/order.rb
@@ -0,0 +1,8 @@
+class Order < ActiveRecord::Base
+
+  scope :unpurchased, -> { where(purchased: false) }
+
+
+  belongs_to :product
+
+end
