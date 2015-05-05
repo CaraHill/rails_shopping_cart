959549aaae668c2df88878a0d6058fd254534f93
Cara Hill <chilly23@gmail.com>
2015-05-05 16:41:46 +1200

    Added tests for product model.

--- shopping_cart/app/models/product.rb
+++ shopping_cart/app/models/product.rb
@@ -1,2 +1,6 @@
 class Product < ActiveRecord::Base
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
+
 end
--- shopping_cart/spec/models/product_spec.rb
+++ shopping_cart/spec/models/product_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
-# RSpec.describe Product, :type => :model do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+RSpec.describe Product, :type => :model do
+  it "creates an instance product" do
+    product = Product.new
+    expect(product).to be_instance_of(Product)
+  end
+
+  describe "product validity" do
+    it "check if a product is invalid" do
+      product = Product.new
+      expect(product).not_to be_valid
+    end
+
+    it "create a valid product" do
+      product = Product.new(name: "Shoe", description: "One shoe.", image: "image", price: 500)
+      expect(product).to be_valid
+    end
+  end
+
+end
